a237af06eb93b41fc3661d59250e2d0ef8462224
shidry <492482580@qq.com>
2019-11-14 16:08:46 +0800

    ..

---
+++ src/main/java/com/imooc/sell/entity/User.java
@@ -0,0 +1,61 @@
+package com.imooc.sell.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author liuc
+ * @since 2019-11-14
+ */
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+            "id=" + id +
+            ", name=" + name +
+            ", age=" + age +
+        "}";
+    }
+}
---
+++ src/main/java/com/imooc/sell/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.imooc.sell.mapper;
+
+import com.imooc.sell.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * <p>
+ * Mapper 接口
+ * </p>
+ *
+ * @author liuc
+ * @since 2019-11-14
+ */
+public interface UserMapper extends BaseMapper<User> {
+    Integer insertBach(@Param("users") List<User> users);
+}
